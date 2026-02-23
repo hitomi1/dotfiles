f08365313f72aab73f110b247053c30ef686a4a7
Gustavo Hitomi <gustavohitomi@gmail.com>
2026-02-23 20:20:33 -0300

    add Homebrew shell environment setup to .zprofile and disable Visual Studio Code installation.

--- setup/setup-macos.sh
+++ setup/setup-macos.sh
@@ -5,6 +5,11 @@ if ! command -v brew &>/dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+# Add Homebrew to shell environment
+echo >> "$HOME/.zprofile"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv zsh)"' >> "$HOME/.zprofile"
+eval "$(/opt/homebrew/bin/brew shellenv zsh)"
+
 # Install CLI tools
 brew install \
   zsh \
@@ -24,7 +29,7 @@ brew install --cask \
   telegram \
   discord \
   spotify \
-  visual-studio-code
+  # visual-studio-code
 
 # Install Nerd Fonts
 brew install --cask \
